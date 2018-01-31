33e1dfa20cb8d4a88dcff4e2b3587348e772e2a0
Kate Fryatt <kateyalice@gmail.com>
2018-01-31 16:02:40 +0000

    Changing the text out for images
    --import Image

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, Image } from 'react-native';
 
 export default class HelloWorldApp extends Component {
   render() {
