56c26f936cbcced9f41e9190ed6c107e8c2f6f0c
Kate Fryatt <kateyalice@gmail.com>
2018-01-31 16:21:38 +0000

    Added image link in

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { AppRegistry, Image } from 'react-native';
 
-export default class HelloWorldApp extends Component {
+export default class TastyFoods extends Component {
   render() {
-    return <Text>Hello world!</Text>;
+    let pic = {
+      uri:
+        'https://lh6.googleusercontent.com/1db5oGNBot28BkzNpoeC5h2hI5DJzkWp3QMgGaEqqN6XaUBvEQm8ToE5-hajSoIRVEpZUPNc7NgrUk4=w3000-h1738-rw',
+    };
+
+    return <Image source={pic} style={{ width: 193, height: 110 }} />;
   }
 }
