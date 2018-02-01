78c7312fad8cd609e19a7434e04a262f27a10a6a
Kate Fryatt <kateyalice@gmail.com>
2018-02-01 15:53:23 +0000

    test

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import { AppRegistry, Image } from 'react-native';
 
 export default class TastyFoods extends Component {
   render() {
-    let pic = {
+    let picture = {
       uri:
         'https://lh6.googleusercontent.com/1db5oGNBot28BkzNpoeC5h2hI5DJzkWp3QMgGaEqqN6XaUBvEQm8ToE5-hajSoIRVEpZUPNc7NgrUk4=w3000-h1738-rw',
     };
 
-    return <Image source={pic} style={{ width: 300, height: 250 }} />;
+    return <Image source={picture} style={{ width: 300, height: 250 }} />;
   }
 }
